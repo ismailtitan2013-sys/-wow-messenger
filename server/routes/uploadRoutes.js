6b34990934154380b25f4bf089d19eb94f88ffd9
ismailtitan <ismail@test.com>
2026-07-30 17:12:07 +0300

    Fix multer uploads path

--- server/routes/uploadRoutes.js
+++ server/routes/uploadRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/');
+    cb(null, path.join(__dirname, '../uploads'));
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + '-' + Math.round(Math.random() * 1E9) + path.extname(file.originalname));
