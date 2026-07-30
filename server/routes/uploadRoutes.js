f56e1353e8d0230cae22affb3264435066d52723
ismailtitan <ismail@test.com>
2026-07-30 17:14:03 +0300

    Auto-create uploads directory on server startup

--- server/routes/uploadRoutes.js
+++ server/routes/uploadRoutes.js
@@ -3,11 +3,18 @@ const multer = require('multer');
 const path = require('path');
 const authMiddleware = require('../middleware/authMiddleware');
 
+const fs = require('fs');
+
 const router = express.Router();
+
+const uploadDir = path.join(__dirname, '../uploads');
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '../uploads'));
+    cb(null, uploadDir);
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + '-' + Math.round(Math.random() * 1E9) + path.extname(file.originalname));
