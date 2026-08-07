8a71d6ec33775dd9e60bec6d4a83fb46ee23dfdb
ismailtitan <ismail@test.com>
2026-08-07 15:18:32 +0300

    Fix base path

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/wow-messenger/',
+  base: '/-wow-messenger/',
   plugins: [react()],
 })
